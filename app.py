f171a257843e206263b89575279af1a9bbf5453e
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2019-04-04 16:29:47 +0530

    refactored the code
    Refractored the code to reuse components.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-    
-@app.route("/about")
-def about():
-    return render_template("about.html")
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")
+
+@app.route("/search", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("failure.html")
+    return render_template("success.html", name=name)
+
