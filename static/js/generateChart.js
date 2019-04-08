c8596e931f38229619da84fc638ff012c8fa8e64
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2019-04-08 19:55:11 +0530

    integrated the entire code to account for predictions

--- static/js/generateChart.js
+++ static/js/generateChart.js
@@ -1,26 +1,7 @@
 // This file contains generateChart which takes in a parameter called dps (datapoints) and plots it using Canvas.js
 
-function generateChart(datapoints) {
-    //length of the dataset
-    let dataset_length = 80;
-
-	//dataPoints. 
-	var dps = [];  //should come from python
-    var pred = []; //the predicted points
-
-	//getting the score table
-    let i = 0;
-	for(i = 0; i < dataset_length; i++) {
-		dps.push({x: i,y: Math.random()*50});
-	}
-    let index = i;
-    pred.push({
-        x: dps[index-1].x, y: dps[index-1].y
-    });
-    for(i = index; i < index + 20; i++) {
-        pred.push({x: i,y: Math.random()*50});
-    }
-				
+function generateChart(dps, pred) {
+		
 	var chart = new CanvasJS.Chart("chartContainer",{
 		title :{
 			text: "Stock Prediction"
--- static/js/predict.js
+++ static/js/predict.js
@@ -1,16 +1,27 @@
 // This file deals with the prediction of stocks using normal linear regression.
 // Created two datasets, datapoints_x and datapoints_y
 
-
+function predictions(dps, dps_length) {
 
     let datapoints_x = [];
     let datapoints_y = [];
+    let pred = []
 
-    for(let i = 0; i < 970; i++) {
-        datapoints_x.push(i);
+    for(let i = 0; i < dps_length; i++) {
+        datapoints_x.push(dps[i].x);
     }
-    for(let i = 0; i < 970; i++) {
-        datapoints_y.push(Math.random());
+    for(let i = 0; i < dps_length; i++) {
+        datapoints_y.push(dps[i].y);
+    }
+
+    let index = dps_length;
+    pred.push({
+        x: dps[index-1].x, y: dps[index-1].y
+    });
+    
+    let vals_to_pred = [];
+    for(let i = index; i < index + 10; i++) {
+        vals_to_pred.push(i);
     }
 
     const xs = tf.tensor1d(datapoints_x);
@@ -20,8 +31,8 @@
     const xmax = xs.max();
     const xrange = xmax.sub(xmin);
 
-    xsNorm = norm(xs)
-    ysNorm = norm(ys)
+    xsNorm = norm(xs, xmin, xrange)
+    ysNorm = norm(ys, xmin, xrange)
 
     const a = tf.variable(tf.scalar(Math.random()));
     const b = tf.variable(tf.scalar(Math.random()));
@@ -38,7 +49,7 @@
 
     for (let iter = 0; iter < numIterations; iter++) {
         optimizer.minimize(() => {
-            const predsYs = predict(xsNorm);
+            const predsYs = predict(xsNorm, a, b);
             const e = loss(predsYs, ysNorm);
             errors.push(e.dataSync())
             return e
@@ -48,21 +59,32 @@
     console.log("errs [0]" + errors[0])
     console.log(errors[numIterations - 1])
 
-    xTest = tf.tensor1d([0.1, 0.2, 0.3, 0.4, 974])
-    predict(xTest).print()
-
+    xTest = tf.tensor1d(vals_to_pred)
+    const predicted_vals = predict(xTest, a, b)
+    predicted_vals.print()
+    const values_returned = Array.from(predicted_vals.dataSync());
+    for(let i = index; i < index+10; i++ ) {
+        pred.push({
+            x: i, y: values_returned[i - dps_length]
+        });
+    }
     a.print()
     b.print()
+    return pred;
+   
+}
+
+    
     
     function loss(predictions, labels) {
         return predictions.sub(labels).square().mean();
     }
     
-    function predict(x) {
+    function predict(x, a, b) {
         return tf.tidy(() => {
             return a.mul(x).add(b)
         });
     }
-    function norm(x) {
+    function norm(x, xmin, xrange) {
         return x.sub(xmin).div(xrange);
     }
