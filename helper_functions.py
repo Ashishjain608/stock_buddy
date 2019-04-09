8d26e8a3f1d8db40edd759e5c201ea4aa4abf7e2
Ashish Jain <ashishjain608@gmail.com>
2019-04-10 01:53:25 +0530

    fetch stock price

--- helper_functions.py
+++ helper_functions.py
@@ -1,26 +1,39 @@
 import requests
 import urllib.parse
 from flask import redirect, render_template, request, session
+import quandl
+import pandas as pd
+quandl.ApiConfig.api_key = 'VbsmWFysHcxq6F1vZNPX'
 
-def lookup(symbol):
+# def lookup(symbol):
 
     # Contact API
-    try:
-        response = requests.get(f"https://api.iextrading.com/1.0/stock/{(symbol)}/chart/1d/quote")
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
+    # try:
+    #     response = requests.get(f"https://api.iextrading.com/1.0/stock/{(symbol)}/chart/1d/quote")
+    #     response.raise_for_status()
+    # except requests.RequestException:
+    #     return None
 
-    # Parse response
+    # # Parse response
+    # try:
+    #     quote=response.json()
+    #     data=[]
+    #     for i in range(len(quote)):
+    #         data.append({
+    #              "open": quote[i]["marketAverage"]
+    #         })
+    #     return {
+    #         "data": data
+    #     }
+    # except (KeyError, TypeError, ValueError):
+    #     return None
+
+def fetch_stock(stock_Id):
     try:
-        quote=response.json()
-        data=[]
-        for i in range(len(quote)):
-            data.append({
-                 "open": quote[i]["marketAverage"]
-            })
+        data = quandl.get("NSE/" + stock_Id)
+        print(data)
         return {
             "data": data
-        }
+        }   
     except (KeyError, TypeError, ValueError):
         return None
